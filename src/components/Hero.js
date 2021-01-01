1a03756306690eabaf152f76af817486fd28ae95
Kevin <35633054+cerlans@users.noreply.github.com>
2021-01-01 10:11:06 -0500

    Add files via upload

---
+++ src/components/Hero.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Hero(){
+  return (
+    <section className='Hero'>
+    <div className ='Hero-content'>
+    <h1>Discover The World</h1>
+    <h2>We offer the best adventure holidays, and tailor made experiences</h2>
+    </div>
+    
+    </section>
+  )
+}
+
+export default Hero
---
+++ src/components/Main.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import AboutMe from './AboutMe.js'
+function Main(){
+return(
+  <AboutMe />
+)
+}
+
+
+
+export default Main
