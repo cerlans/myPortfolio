fb0a39dcd5b4ed074756dba4383c5d228f68c569
Kevin <35633054+cerlans@users.noreply.github.com>
2021-01-01 10:07:16 -0500

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+import Header from './Header.js'
+import Main from './Main.js'
+export default function App() {
+  return (
+    <div>
+      <Header />
+      <Main />
+    </div>
+  );
+}
