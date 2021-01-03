94bf79b364077b9a701e7b5e770cbf324914590e
Kevin <35633054+cerlans@users.noreply.github.com>
2021-01-02 22:11:37 -0500

    Create data.js

---
+++ src/components/data.js
@@ -0,0 +1,28 @@
+/* Data for the about me cards section*/
+/* Might store some more later*/
+const data = [{
+id: 1,
+icon:'https://i.vgy.me/eOnJlb.png',
+
+},{
+id: 2,
+icon:'https://i.vgy.me/1gXQ61.png',
+desc:'CSS3'
+},{
+id:3,
+icon:'https://i.vgy.me/MaJEhe.png',
+
+},{
+id: 4,
+icon:'https://i.vgy.me/SfNmd8.png',
+
+},{
+id: 5,
+icon:'https://i.vgy.me/efj8cH.png',
+
+}];
+
+
+console.log(data)
+
+export default data
