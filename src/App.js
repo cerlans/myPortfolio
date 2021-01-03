6fa868b6707cbcdcb8a585187790ff9f1eb93efe
Kevin <35633054+cerlans@users.noreply.github.com>
2021-01-03 12:35:03 -0500

    Update App.js
    changed react parent div's to react-fragments

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import Header from './Header.js'
 import Main from './Main.js'
 export default function App() {
   return (
-    <div>
+    <>
       <Header />
       <Main />
-    </div>
+    </>
   );
 }
