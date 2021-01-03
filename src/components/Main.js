fdb8a6d105eefc5f0dd7d7e3d340e613746697f2
Kevin <35633054+cerlans@users.noreply.github.com>
2021-01-03 12:40:23 -0500

    Update Main.js
    document fragment added, array returning '<Project/>' component is mapped to my project data in data.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,8 +1,20 @@
 import React from 'react'
 import AboutMe from './AboutMe.js'
+import projectData from './data.js'
+import Projects from './Projects.js'
+
+
 function Main(){
+ const pro = projectData.map(function(value){
+   return (
+     <Projects key = {value.id} image = {value.image} title={value.title} desc={value.description}/>
+   )
+ })
 return(
+  <>
   <AboutMe />
+  {pro}
+  </>
 )
 }
 
