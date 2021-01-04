d2a2722961d1e07fc69f33e693fcc08c40c5b1d5
Kevin <35633054+cerlans@users.noreply.github.com>
2021-01-04 11:40:19 -0500

    Create Header.js
    Main header component

---
+++ src/header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from './Navbar.js'
+import Hero from './Hero.js'
+function Header(){
+  return (
+    <>
+    <Navbar />
+    <Hero />
+    </>
+  )
+}
+
+export default Header
