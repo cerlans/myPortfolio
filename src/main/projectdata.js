20d037c171903c0dca06432fda0a48d8439c7c29
Kevin <35633054+cerlans@users.noreply.github.com>
2021-01-04 11:47:14 -0500

    Create projectdata.js

---
+++ src/main/projectdata.js
@@ -0,0 +1,18 @@
+const projectData =[{
+  id:1,
+  title:'JukeBox',
+  image:'https://picsum.photos/200',
+  description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sapien lectus, ultricies nec tortor eget, placerat varius augue. Integer vehicula urna in orci lacinia vestibulum eu a ipsum. Quisque auctor, libero id commodo auctor, dolor mauris porttitor lectus, at interdum urna velit nec sem. Pellentesque sit amet mattis ipsum. Suspendisse dapibus fringilla quam, nec hendrerit lectus eleifend tincidunt. Aliquam lacinia mi sagittis justo ultrices mollis. '
+},{
+   id:2,
+   title:'Entry Level',
+   image:'https://picsum.photos/200',
+  description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sapien lectus, ultricies nec tortor eget, placerat varius augue. Integer vehicula urna in orci lacinia vestibulum eu a ipsum. Quisque auctor, libero id commodo auctor, dolor mauris porttitor lectus, at interdum urna velit nec sem. Pellentesque sit amet mattis ipsum. Suspendisse dapibus fringilla quam, nec hendrerit lectus eleifend tincidunt. Aliquam lacinia mi sagittis justo ultrices mollis.'
+},{
+   id:3,
+   title:'Weather Application',
+  image:'https://picsum.photos/200',
+  description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sapien lectus, ultricies nec tortor eget, placerat varius augue. Integer vehicula urna in orci lacinia vestibulum eu a ipsum. Quisque auctor, libero id commodo auctor, dolor mauris porttitor lectus, at interdum urna velit nec sem. Pellentesque sit amet mattis ipsum. Suspendisse dapibus fringilla quam, nec hendrerit lectus eleifend tincidunt. Aliquam lacinia mi sagittis justo ultrices mollis. '
+}]
+
+export default projectData
