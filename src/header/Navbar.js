1e582eb9fc7eaf0105f95bb2df2ebd1c87e6c4a6
Kevin <35633054+cerlans@users.noreply.github.com>
2021-01-04 11:41:24 -0500

    Create Navbar.js
    Reorganized file structure, previous was too confusing

---
+++ src/header/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Navbar () {
+  return (
+  <nav className='nav-cont'>
+    <div>
+    <h1>K.G</h1>
+    </div>
+      <div className='bars'>
+      <i className="fas fa-bars"></i>
+      </div>
+   </nav>
+  )
+}
+
+export default Navbar
