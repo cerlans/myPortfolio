9300a80dded74175ab31c1cd1f1e9c41086a3160
Kevin <35633054+cerlans@users.noreply.github.com>
2021-01-04 11:46:00 -0500

    Create skillsData.js

---
+++ src/main/skillsData.js
@@ -0,0 +1,26 @@
+/* Data for the about me cards section*/
+/* Might store some more later*/
+const data = [{
+id: 1,
+icon:'https://i.vgy.me/eOnJlb.png',
+
+},{
+id: 2,
+icon:'https://i.vgy.me/1gXQ61.png',
+desc:'CSS3'
+},{
+id:3,
+icon:'https://i.vgy.me/MaJEhe.png',
+
+},{
+id: 4,
+icon:'https://i.vgy.me/SfNmd8.png',
+
+},{
+id: 5,
+icon:'https://i.vgy.me/efj8cH.png',
+
+}];
+
+export default data;
+
