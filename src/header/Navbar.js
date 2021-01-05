fde4259c5feabe726396bd3bf9b4c08b88449eca
Kevin <35633054+cerlans@users.noreply.github.com>
2021-01-05 12:17:14 -0500

    Update Navbar.js
    added fragment and sidebar component

--- src/header/Navbar.js
+++ src/header/Navbar.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 function Navbar () {
   return (
+    <>
   <nav className='nav-cont'>
     <div>
     <h1>K.G</h1>
@@ -10,6 +11,18 @@ function Navbar () {
       <i className="fas fa-bars"></i>
       </div>
    </nav>
+    <nav className='sidenav'>
+    <div className='items'>
+    <ul>
+      <li><i class="fas fa-times"></i></li>
+      <li>Home</li>
+      <li>About</li>
+      <li>Projects</li>
+      <li>Contact</li>
+    </ul>
+    </div>
+   </nav>
+   </>
   )
 }
 
