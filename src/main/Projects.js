e81eedc4107ee878918230e01e8392e2a0f35387
Kevin <35633054+cerlans@users.noreply.github.com>
2021-01-07 13:11:19 -0500

    Update Projects.js
    added a modal pop-up, which will only be displayed when the button off 'modal-btn' is clicked (line 17).

--- src/main/Projects.js
+++ src/main/Projects.js
@@ -3,7 +3,7 @@ import projectData from './projectdata.js'
 console.log(projectData)
 function Projects(props) {
 return(
-  <section className='projects'>
+ <section className='projects'>
     <div className='col'>
       <div className='imgcontainer'>
       <img src={props.image}/>
@@ -14,9 +14,41 @@ return(
       </div>
       <div className='footer'>
         <button>Live Demo</button>
-        <button class="view">View More</button>
+        <button className="modal-btn" onClick = {test}>View More</button>
       </div>
     </div>
+
+{/* Below this line is a modal that will appear when the 'view more' button is clicked */}
+  <div className="modal">
+     <div className="modal-content">
+        <div className="modal-header">
+           <span className="close-btn">&times;</span>
+           <h1>{props.title}</h1>
+        </div>
+        <div className ='modal-inner'>
+           <img src = {props.image}/>
+           <p>{props.description}</p>
+           <h3>Tech Stacks</h3>
+           <ul>
+              <li>
+                 React
+              </li>
+              <li>
+                 React
+              </li>
+              <li>
+                 React
+              </li>
+              <li>
+                 React
+              </li>
+           </ul>
+        </div>
+        <div className="modal-footer">
+           <button>Live Demo</button>
+        </div>
+     </div>
+  </div>
   </section>
 )
 }
