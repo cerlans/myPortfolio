a2ee94ffb0b41bb2813eb25f2596a0e578783fcf
Kevin <35633054+cerlans@users.noreply.github.com>
2021-01-07 10:28:25 -0500

    Update Navbar.js
    added a sideNavBar for mobile screens, event listener to trigger the sideNav.

--- src/header/Navbar.js
+++ src/header/Navbar.js
@@ -1,20 +1,32 @@
 import React from 'react';
 
 function Navbar () {
+ const [currentClass,setClass] = useState('sidenav');
+ function Click(){
+   if(currentClass === 'sidenav'){
+     setClass('repnav')
+        document.body.style.overflow = 'hidden'
+
+   }else {
+     setClass('sidenav')
+     document.body.style.overflow = 'visible'
+
+   }
+ }
   return (
     <>
   <nav className='nav-cont'>
     <div>
     <h1>K.G</h1>
     </div>
-      <div className='bars'>
+      <div className='bars' onClick={Click}>
       <i className="fas fa-bars"></i>
       </div>
    </nav>
-    <nav className='sidenav'>
+   <nav className={currentClass}>
     <div className='items'>
     <ul>
-      <li><i class="fas fa-times"></i></li>
+      <span><i className="fas fa-times" onClick={Click}></i></span>
       <li>Home</li>
       <li>About</li>
       <li>Projects</li>
