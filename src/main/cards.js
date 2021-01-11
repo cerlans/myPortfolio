13262327b5d54dc8d47c00645e75048f91a36e98
Kevin <35633054+cerlans@users.noreply.github.com>
2021-01-11 12:51:13 -0500

    Update cards.js
    removed unneeded elements

--- src/main/cards.js
+++ src/main/cards.js
@@ -2,18 +2,11 @@ import React from "react";
 
 function Cards(props){
 return (
-   <div className = 'container'>
-   <div className ='row'>
+   
     <div className='hvr-fade'>
     <img src={props.icon} />  
     </div>
-    </div>
-       <div className ='row'>
-    <div className='hvr-fade'>
-    <img src={props.icon} />  
-    </div>
-    </div>  
-    </div>
+    
     )
 }
 
