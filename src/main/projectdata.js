d8184b0426aabcecbd6047a7c3394a060822850d
Kevin <35633054+cerlans@users.noreply.github.com>
2021-01-18 12:18:12 -0500

    Update projectdata.js
    added more filler data to see how it would effect the wrapping

--- src/main/projectdata.js
+++ src/main/projectdata.js
@@ -1,18 +1,31 @@
 const projectData =[{
   id:1,
-  title:'JukeBox',
+  title:'Juke-Box',
   image:'https://picsum.photos/200',
-  description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sapien lectus, ultricies nec tortor eget, placerat varius augue. Integer vehicula urna in orci lacinia vestibulum eu a ipsum. Quisque auctor, libero id commodo auctor, dolor mauris porttitor lectus, at interdum urna velit nec sem. Pellentesque sit amet mattis ipsum. Suspendisse dapibus fringilla quam, nec hendrerit lectus eleifend tincidunt. Aliquam lacinia mi sagittis justo ultrices mollis. '
+  description:'Web Resources is an application that allows beginner and intermediate programmers to share learning resources such as online courses and tutorials. '
 },{
    id:2,
    title:'Entry Level',
    image:'https://picsum.photos/200',
-  description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sapien lectus, ultricies nec tortor eget, placerat varius augue. Integer vehicula urna in orci lacinia vestibulum eu a ipsum. Quisque auctor, libero id commodo auctor, dolor mauris porttitor lectus, at interdum urna velit nec sem. Pellentesque sit amet mattis ipsum. Suspendisse dapibus fringilla quam, nec hendrerit lectus eleifend tincidunt. Aliquam lacinia mi sagittis justo ultrices mollis.'
+  description:'Web Resources is an application that allows beginner and intermediate programmers to share learning resources such as online courses and tutorials.'
 },{
    id:3,
    title:'Weather Application',
   image:'https://picsum.photos/200',
-  description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sapien lectus, ultricies nec tortor eget, placerat varius augue. Integer vehicula urna in orci lacinia vestibulum eu a ipsum. Quisque auctor, libero id commodo auctor, dolor mauris porttitor lectus, at interdum urna velit nec sem. Pellentesque sit amet mattis ipsum. Suspendisse dapibus fringilla quam, nec hendrerit lectus eleifend tincidunt. Aliquam lacinia mi sagittis justo ultrices mollis. '
+  description:'Web Resources is an application that allows beginner and intermediate programmers to share learning resources such as online courses and tutorials.'
+},{
+   id:3,
+   title:'Weather Application',
+  image:'https://picsum.photos/200',
+  description:'Web Resources is an application that allows beginner and intermediate programmers to share learning resources such as online courses and tutorials.'
+},{
+   id:3,
+   title:'Weather Application',
+  image:'https://picsum.photos/200',
+  description:'Web Resources is an application that allows beginner and intermediate programmers to share learning resources such as online courses and tutorials.'
+},{
+   id:3,
+   title:'Weather Application',
+  image:'https://picsum.photos/200',
+  description:'Web Resources is an application that allows beginner and intermediate programmers to share learning resources such as online courses and tutorials.'
 }]
-
-export default projectData
